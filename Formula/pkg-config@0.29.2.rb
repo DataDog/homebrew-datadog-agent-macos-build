dbf35e7c88fedab629c8b21135507c24476af956
Alexandre Menasria <alexandre.menasria@datadoghq.com>
2022-11-08 12:08:48 +0100

    Change license from Apache 2 to BSD-2-Clause

--- Formula/pkg-config@0.29.2.rb
+++ Formula/pkg-config@0.29.2.rb
@@ -1,8 +1,3 @@
-# Unless explicitly stated otherwise all files in this repository are licensed
-# under the Apache License Version 2.0.
-# This product includes software developed at Datadog (https://www.datadoghq.com/)
-# Copyright 2022-present Datadog, Inc.
-
 class PkgConfigAT0292 < Formula
   desc "Manage compile and link flags for libraries"
   homepage "https://freedesktop.org/wiki/Software/pkg-config/"
--- Formula/python@3.8.11.rb
+++ Formula/python@3.8.11.rb
@@ -1,8 +1,3 @@
-# Unless explicitly stated otherwise all files in this repository are licensed
-# under the Apache License Version 2.0.
-# This product includes software developed at Datadog (https://www.datadoghq.com/)
-# Copyright 2022-present Datadog, Inc.
-
 class PythonAT3811 < Formula
   desc "Interpreted, interactive, object-oriented programming language"
   homepage "https://www.python.org/"
