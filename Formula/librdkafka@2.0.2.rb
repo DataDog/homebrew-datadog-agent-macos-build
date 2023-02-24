9fbe334f16cea2b4d95006a6c682af880baa947a
Florent Clarret <florent.clarret@datadoghq.com>
2023-02-24 08:37:39 +0100

    Add the librdkafka formula

---
+++ Formula/librdkafka@2.0.2.rb
@@ -0,0 +1,45 @@
+class LibrdkafkaAT202 < Formula
+  desc "Apache Kafka C/C++ library"
+  homepage "https://github.com/edenhill/librdkafka"
+  url "https://github.com/edenhill/librdkafka/archive/refs/tags/v2.0.2.tar.gz"
+  sha256 "f321bcb1e015a34114c83cf1aa7b99ee260236aab096b85c003170c90a47ca9d"
+  license "BSD-2-Clause"
+  head "https://github.com/edenhill/librdkafka.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "python@3.11" => :build
+  depends_on "lz4"
+  depends_on "lzlib"
+  depends_on "openssl@1.1"
+  depends_on "zstd"
+
+  uses_from_macos "curl"
+  uses_from_macos "cyrus-sasl"
+  uses_from_macos "zlib"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <librdkafka/rdkafka.h>
+
+      int main (int argc, char **argv)
+      {
+        int partition = RD_KAFKA_PARTITION_UA; /* random */
+        int version = rd_kafka_version();
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-lrdkafka", "-lz", "-lpthread", "-o", "test"
+    system "./test"
+  end
+end
